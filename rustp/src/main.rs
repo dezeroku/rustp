4a68e7ebd5fd6739aca0a7e7778a6db7c53f3363
d0ku <darthtyranus666666@gmail.com>
2021-02-16 21:29:37 +0100

    multiple expressions

--- rustp/src/main.rs
+++ rustp/src/main.rs
@@ -31,6 +31,23 @@ fn fun1() {
     assert!(fun::ExprParser::new().parse("3 -").is_err());
 }
 
+#[test]
+fn fun2() {
+    assert!(fun::ExprsParser::new().parse("2").is_ok());
+}
+
+#[test]
+fn fun3() {
+    assert!(fun::ExprsParser::new().parse("2 + 2 \n 3 - 1").is_ok());
+}
+
+#[test]
+fn fun4() {
+    assert!(fun::ExprsParser::new()
+        .parse("2 + 2 \n 3 - 1 \n 12")
+        .is_ok());
+}
+
 fn check(t: &str) -> i32 {
     let temp = calculator::TermParser::new().parse(t);
     if temp.is_ok() {
@@ -40,13 +57,28 @@ fn check(t: &str) -> i32 {
     }
 }
 
+use std::fs;
 fn main() {
-    println!("Hello, world!");
-    println!("{}", check("22"));
-    println!("{}", check("(22"));
-    println!("{}", check("(22)"));
-    println!("{}", check("(((22)))"));
-    println!("{}", unpack(fun::ExprParser::new().parse("2").unwrap()));
-    println!("{}", unpack(fun::ExprParser::new().parse("2+2").unwrap()));
-    println!("{}", unpack(fun::ExprParser::new().parse("2-2").unwrap()));
+    // Open a file
+    // Read its content
+    // Write it down in prefix notation
+    let filename = "example";
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+
+    //println!("Hello, world!");
+    //println!("{}", check("22"));
+    //println!("{}", check("(22"));
+    //println!("{}", check("(22)"));
+    //println!("{}", check("(((22)))"));
+    //println!("{}", unpack(fun::ExprParser::new().parse("2").unwrap()));
+    //println!("{}", unpack(fun::ExprParser::new().parse("2+2").unwrap()));
+    //println!("{}", unpack(fun::ExprsParser::new().parse("2-2").unwrap()));
+
+    for item in fun::ExprsParser::new().parse(&contents).unwrap().iter() {
+        println!("{}", item.to_string());
+    }
 }
