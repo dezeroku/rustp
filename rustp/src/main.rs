43079d94fb358d4c2838c8679e395af08f4360eb
d0ku <darthtyranus666666@gmail.com>
2021-02-19 02:39:25 +0100

    dirty working expressions

--- rustp/src/main.rs
+++ rustp/src/main.rs
@@ -2,7 +2,11 @@ mod ast;
 mod parser;
 
 use std::fs;
+
 fn main() {
+    #[cfg(debug_assertions)]
+    println!("Running a DEBUG version");
+
     // Open a file
     // Read its content
     // Write it down in prefix notation
@@ -10,8 +14,7 @@ fn main() {
     println!("In file {}:", filename);
 
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    let t = parser::expr_expr(&contents).expect("Ooops");
+    let t = parser::expr(&contents).expect("Ooops");
     //println!("With text:\n{}", contents);
-    println!("Left: {}, Got: {}", t.0, t.1);
-    //println!("Hello, world!");
+    println!("Left: |{}|, Got: |{}|", t.0, t.1);
 }
--- rustp/src/parser.rs
+++ rustp/src/parser.rs
@@ -3,9 +3,10 @@ extern crate nom;
 use crate::ast;
 
 use nom::{
-    branch::alt, bytes::complete::take_while1, character::complete::digit1,
-    character::complete::one_of, character::complete::space0, character::is_digit,
-    combinator::map_res, combinator::opt, map, named, sequence::tuple, take_while, IResult,
+    branch::alt, bytes::complete::take_while1, character::complete::char,
+    character::complete::digit1, character::complete::one_of, character::complete::space0,
+    character::is_digit, combinator::map_res, combinator::opt, error::ErrorKind, map,
+    multi::fold_many1, multi::many0, multi::many1, named, sequence::tuple, take_while, IResult,
 };
 use std::str::FromStr;
 
@@ -21,11 +22,81 @@ fn digito(input: &str) -> IResult<&str, &str> {
 //    match t
 //}
 
-pub fn expr_expr(input: &str) -> IResult<&str, Box<ast::Expr>> {
-    tuple((expr_number, space0, opcode, space0, expr_number))(input).map(|(next_input, res)| {
-        let (a, _, op, _, b) = res;
-        (next_input, Box::new(ast::Expr::Op(a, op, b)))
+// concept: first answer to https://stackoverflow.com/questions/59508862/using-parser-combinator-to-parse-simple-math-expression
+pub fn expr(input: &str) -> IResult<&str, Box<ast::Expr>> {
+    expr_expr(input)
+}
+
+fn primary_expr(input: &str) -> IResult<&str, Box<ast::Expr>> {
+    expr_number(input).or_else(|e| {
+        char('(')(input)
+            .and_then(|(next_input, _)| space0(next_input))
+            .and_then(|(next_input, _)| expr_expr(next_input))
+            .and_then(|(next_input, res)| {
+                space0(next_input)
+                    .and_then(|(next_input, _)| char(')')(next_input))
+                    .and_then(|(next_input, _)| space0(next_input))
+                    .and_then(|(next_input, _)| Ok((next_input, res)))
+            })
     })
+}
+
+fn mult_expr_right(input: &str) -> IResult<&str, (ast::Opcode, Box<ast::Expr>)> {
+    tuple((space0, mult_or_divide, space0, primary_expr, space0))(input).and_then(
+        |(next_input, x)| {
+            let (_, op, _, b, _) = x;
+            Ok((next_input, (op, b)))
+        },
+    )
+}
+
+pub fn mult_expr(input: &str) -> IResult<&str, Box<ast::Expr>> {
+    space0(input)
+        .and_then(|(next_input, _)| primary_expr(next_input))
+        .and_then(|(next_input, a)| {
+            let f = many0(mult_expr_right)(next_input);
+            match f {
+                Ok(x) => {
+                    let (next_input, vect) = x;
+                    let mut temp = a;
+                    for item in vect {
+                        let (op, b) = item;
+                        temp = Box::new(ast::Expr::Op(temp, op, b));
+                    }
+                    Ok((next_input, temp))
+                }
+                Err(_) => Ok((next_input, a)),
+            }
+        })
+}
+
+fn add_expr_right(input: &str) -> IResult<&str, (ast::Opcode, Box<ast::Expr>)> {
+    tuple((space0, add_or_subtract, space0, mult_expr, space0))(input).and_then(
+        |(next_input, x)| {
+            let (_, op, _, b, _) = x;
+            Ok((next_input, (op, b)))
+        },
+    )
+}
+
+pub fn expr_expr(input: &str) -> IResult<&str, Box<ast::Expr>> {
+    space0(input)
+        .and_then(|(next_input, _)| mult_expr(next_input))
+        .and_then(|(next_input, a)| {
+            let f = many0(add_expr_right)(next_input);
+            match f {
+                Ok(x) => {
+                    let (next_input, vect) = x;
+                    let mut temp = a;
+                    for item in vect {
+                        let (op, b) = item;
+                        temp = Box::new(ast::Expr::Op(temp, op, b));
+                    }
+                    Ok((next_input, temp))
+                }
+                Err(_) => Ok((next_input, a)),
+            }
+        })
 }
 
 fn expr_number(input: &str) -> IResult<&str, Box<ast::Expr>> {
@@ -38,8 +109,8 @@ fn expr_number(input: &str) -> IResult<&str, Box<ast::Expr>> {
     }
 }
 
-fn opcode(input: &str) -> IResult<&str, ast::Opcode> {
-    let t = one_of("*+-/")(input);
+fn mult_or_divide(input: &str) -> IResult<&str, ast::Opcode> {
+    let t = one_of("*/")(input);
     match t {
         Ok(a) => {
             let (next_input, res) = a;
@@ -47,8 +118,6 @@ fn opcode(input: &str) -> IResult<&str, ast::Opcode> {
                 next_input,
                 match res {
                     '*' => ast::Opcode::Mul,
-                    '+' => ast::Opcode::Add,
-                    '-' => ast::Opcode::Sub,
                     '/' => ast::Opcode::Div,
                     _ => unimplemented!(),
                 },
@@ -58,12 +127,131 @@ fn opcode(input: &str) -> IResult<&str, ast::Opcode> {
     }
 }
 
+fn add_or_subtract(input: &str) -> IResult<&str, ast::Opcode> {
+    let t = one_of("+-")(input);
+    match t {
+        Ok(a) => {
+            let (next_input, res) = a;
+            Ok((
+                next_input,
+                match res {
+                    '+' => ast::Opcode::Add,
+                    '-' => ast::Opcode::Sub,
+                    _ => unimplemented!(),
+                },
+            ))
+        }
+        Err(a) => Err(a),
+    }
+}
+
+#[test]
+fn mult_expr_right1() {
+    assert!(mult_expr_right("13").is_err());
+    assert!(mult_expr_right("*13").is_ok());
+    assert!(
+        mult_expr_right("/13") == Ok(("", (ast::Opcode::Div, Box::new(ast::Expr::Number(13)))))
+    );
+}
+
+#[test]
+fn add_expr_right1() {
+    assert!(add_expr_right("13").is_err());
+    assert!(add_expr_right("-13").is_ok());
+    assert!(add_expr_right("+13") == Ok(("", (ast::Opcode::Add, Box::new(ast::Expr::Number(13))))));
+}
+
+#[test]
+fn mult_expr1() {
+    assert!(
+        mult_expr("13 * 4")
+            == Ok((
+                "",
+                Box::new(ast::Expr::Op(
+                    Box::new(ast::Expr::Number(13)),
+                    ast::Opcode::Mul,
+                    Box::new(ast::Expr::Number(4))
+                ))
+            ))
+    );
+}
+
+#[test]
+fn mult_expr2() {
+    assert!(
+        mult_expr(" 13 * 4")
+            == Ok((
+                "",
+                Box::new(ast::Expr::Op(
+                    Box::new(ast::Expr::Number(13)),
+                    ast::Opcode::Mul,
+                    Box::new(ast::Expr::Number(4))
+                ))
+            ))
+    );
+}
+
+#[test]
+fn mult_expr3() {
+    assert!(
+        mult_expr("13 * 4 ")
+            == Ok((
+                "",
+                Box::new(ast::Expr::Op(
+                    Box::new(ast::Expr::Number(13)),
+                    ast::Opcode::Mul,
+                    Box::new(ast::Expr::Number(4))
+                ))
+            ))
+    );
+}
+
+#[test]
+fn primary_expr1() {
+    assert!(primary_expr("1").is_ok());
+    assert!(primary_expr("3").is_ok());
+    assert!(expr_number("13") == Ok(("", Box::new(ast::Expr::Number(13)))));
+    assert!(primary_expr("(3)").unwrap().0 == "");
+    assert!(primary_expr("").is_err());
+}
+
+#[test]
+fn primary_expr2() {
+    assert!(primary_expr("1 + 1").is_ok());
+    assert!(primary_expr("3 - 1").is_ok());
+    assert!(primary_expr("1 / 1").is_ok());
+    assert!(primary_expr("1 /") == Ok((" /", Box::new(ast::Expr::Number(1)))));
+    assert!(
+        *expr_expr("12 * 3").unwrap().1
+            == *Box::new(ast::Expr::Op(
+                Box::new(ast::Expr::Number(12)),
+                ast::Opcode::Mul,
+                Box::new(ast::Expr::Number(3))
+            ))
+    );
+}
+
+#[test]
+fn primary_expr3() {
+    assert!(primary_expr("1 - 1 + 1").is_ok());
+    assert!(primary_expr("1 * 2 -3").is_ok());
+    assert!(primary_expr("1 + 1 - 3").is_ok());
+    //    assert!(
+    //        *expr_expr("1 - 2 * 3").unwrap().1
+    //            == *Box::new(ast::Expr::Op(
+    //                Box::new(ast::Expr::Number(12)),
+    //                ast::Opcode::Mul,
+    //                Box::new(ast::Expr::Number(3))
+    //            ))
+    //    );
+}
+
 #[test]
 fn expr_expr1() {
     assert!(expr_expr("1 + 1").is_ok());
     assert!(expr_expr("13 - 4").is_ok());
     assert!(expr_expr("13-4").is_ok());
-    assert!(expr_expr("13 / ").is_err());
+    assert!(expr_expr("13 / ").unwrap().0 != "");
 }
 
 #[test]
@@ -79,18 +267,28 @@ fn expr_expr2() {
 }
 
 #[test]
+fn expr_expr3() {
+    assert!(expr_expr("13 * 4 + 2 / 4 - 8").unwrap().0 == "");
+}
+
+#[test]
 fn expr_number1() {
     assert!(expr_number("1").is_ok());
     assert!(*expr_number("13").unwrap().1 == *Box::new(ast::Expr::Number(13)));
 }
 
 #[test]
-fn opcode1() {
-    assert!(opcode("*").is_ok());
-    assert!(opcode("-").is_ok());
-    assert!(opcode("/").is_ok());
-    assert!(opcode("+").unwrap().1 == ast::Opcode::Add);
-    assert!(opcode("8").is_err());
+fn mult_or_divide1() {
+    assert!(mult_or_divide("*").is_ok());
+    assert!(mult_or_divide("/").is_ok());
+    assert!(mult_or_divide("+").is_err());
+}
+
+#[test]
+fn add_or_subtract1() {
+    assert!(add_or_subtract("+").is_ok());
+    assert!(add_or_subtract("-").is_ok());
+    assert!(add_or_subtract("/").is_err());
 }
 
 fn number(input: &str) -> IResult<&str, i32> {
