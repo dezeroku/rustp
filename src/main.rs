d86a3c20d163ac04e10e93ce13ee09e9d6ad4ef3
d0ku <darthtyranus666666@gmail.com>
2021-03-07 01:25:26 +0100

    add simplifier module stub

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod ast;
 mod parser;
+mod simplifier;
 
 use std::fs;
 use std::process::{Command, Stdio};
@@ -34,35 +35,12 @@ fn main() {
 
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
     let t = parser::program(&contents).expect("Ooops");
+    let simplified = simplifier::simplify(t.1);
     println!("Left: |{}|", t.0);
 
-    // OR
-    println!("Got: |{:#?}|", t.1);
+    println!("Got: |{:#?}|", simplified);
     println!("Left: |{}|", t.0);
-    println!("Got: |{:?}|", t.1);
+    println!("Got: |{:?}|", simplified);
 
-    // OR
-    //let copy = t.1.clone();
-    //for item in t.1 {
-    //    println!("|{}|", item);
-    //}
-
-    //let mut temp: String = String::new();
-    //for item in copy {
-    //    temp += &item.to_string();
-    //}
-
-    //temp += &"(check-sat);".to_string();
-
-    //// Only for testing
-    //temp += &"(get-model);".to_string();
-
-    //let mut val: String = String::new();
-    //for item in temp.split(";") {
-    //    val += &item.to_string();
-    //    val += &"\n".to_string();
-    //}
-
-    //println!("{}", val);
     //fs::write("./example.z3", val).expect("Unable to write file");
 }
---
+++ src/simplifier.rs
@@ -0,0 +1,7 @@
+use crate::ast;
+
+/// Infer types for all the bindings
+pub fn simplify(program: ast::Program) -> ast::Program {
+    // TODO: implement
+    program
+}
