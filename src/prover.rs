bd849cc9c75f0b187bd6054f2c29a3eaf5a5baa0
d0ku <darthtyranus666666@gmail.com>
2021-03-16 22:12:03 +0100

    added basic types for prover context

--- src/prover.rs
+++ src/prover.rs
@@ -1,9 +1,20 @@
-use crate::ast;
+use crate::ast::*;
+use std::collections::HashMap;
 
-pub fn prove(input: ast::Program) {
+pub fn prove(input: Program) {
     // Create context for each command and try to prove it individually?
     // All that we have to prove are assertions, all the rest just modifies context.
 
     // For now just display everything here when it happens.
     println!("HAPPENS!");
+}
+
+struct Context {
+    command: Command,
+    state: HashMap<Variable, Val>,
+}
+
+struct Val {
+    v: Value,
+    t: Type,
 }
