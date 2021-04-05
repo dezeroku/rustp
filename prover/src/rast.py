15f4a68b537c025e179205fe5d296cc25a86c2e8
d0ku <darthtyranus666666@gmail.com>
2021-04-06 00:49:42 +0200

    change rast to dict based implementation

--- prover/src/rast.py
+++ prover/src/rast.py
@@ -1,6 +1,6 @@
 import typing
 
-class Base:
+class Base(dict):
     pass
 
 class Bool(Base):
@@ -36,84 +36,75 @@ class Block(Base):
 class ProveControl(Base):
     pass
 
-class Function:
+class Function(Base):
     def __init__(self, name: str, content: typing.List[Command], input:
                  typing.List[Binding], output: Type, precondition: Bool,
                  postcondition: Bool, return_value: Value):
-        self.name = name
-        self.content = content
-        self.input = input
-        self.output = output
-        self.precondition = precondition
-        self.postcondition = postcondition
-        self.return_value = return_value
+        dict.__init__(self, name=name, content=content, input=input,
+                      output=output, precondition=precondition,
+                      postcondition=postcondition, return_value=return_value)
 
 class Program(Base):
     def __init__(self, content: typing.List[Function]):
-        self.content = content
+        dict.__init__(self, content=content)
 
+# TODO: convert all __init__s to use dict.__init__ so it's easily serializable
+# and debug readable
 class BAnd(Bool):
     def __init__(self, a: Bool, b: Bool):
-        self.a = a
-        self.b = b
+        dict.__init__(self, a=a, b=b)
 
 class BOr(Bool):
     def __init__(self, a: Bool, b: Bool):
-        self.a = a
-        self.b = b
+        dict.__init__(self, a=a, b=b)
 
 class BNot(Bool):
     def __init__(self, a: Bool):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class BValue(Bool):
     def __init__(self, a: Value):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class BTrue(Bool):
     def __init__(self):
-        self.a = True
+        dict.__init__(self, a=True)
 
 class BFalse(Bool):
     def __init__(self):
-        self.a = False
+        dict.__init__(self, a=False)
 
 class BEqual(Bool):
     def __init__(self, a: Expr, b: Expr):
-        self.a = a
-        self.b = b
+        dict.__init__(self, a=a, b=b)
 
 class BGreaterThan(Bool):
     def __init__(self, a: Expr, b: Expr):
-        self.a = a
-        self.b = b
+        dict.__init__(self, a=a, b=b)
 
 class BLowerThan(Bool):
     def __init__(self, a: Expr, b: Expr):
-        self.a = a
-        self.b = b
+        dict.__init__(self, a=a, b=b)
 
 class BGreaterEqual(Bool):
     def __init__(self, a: Expr, b: Expr):
-        self.a = a
-        self.b = b
+        dict.__init__(self, a=a, b=b)
 
 class BLowerEqual(Bool):
     def __init__(self, a: Expr, b: Expr):
-        self.a = a
-        self.b = b
+        dict.__init__(self, a=a, b=b)
 
 class PAssert(ProveControl):
     def __init__(self, a: Bool):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class PAssume(ProveControl):
     def __init__(self, a: Bool):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class PLoopInvariant(ProveControl):
     def __init__(self, a: Bool):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class TBool(Type):
     pass
@@ -123,20 +114,19 @@ class TI32(Type):
 
 class TTuple(Type):
     def __init__(self, a: typing.List[Type]):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class TReference(Type):
     def __init__(self, a: Type):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class TReferenceMutable(Type):
     def __init__(self, a: Type):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class TArray(Type):
     def __init__(self, a: Type, length: int):
-        self.a = a
-        self.length = length
+        dict.__init__(self, a=a, length=length)
 
 class TUnit(Type):
     pass
@@ -146,40 +136,39 @@ class TUnknown(Type):
 
 class VExpr(Value):
     def __init__(self, a: Expr):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class VBool(Value):
     def __init__(self, a: Bool):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class VVariable(Value):
     def __init__(self, a: Variable):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class VTuple(Value):
     def __init__(self, a: typing.List[Value]):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class VArray(Value):
     def __init__(self, a: typing.List[Value]):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class VFunctionCall(Value):
     def __init__(self, name: str, args: typing.List[Value]):
-        self.name = name
-        self.args = args
+        dict.__init__(self, name=name, args=args)
 
 class VDereference(Value):
     def __init__(self, a: Value):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class VReference(Value):
     def __init__(self, a: Value):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class VReferenceMutable(Value):
     def __init__(self, a: Value):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class VUnit(Value):
     pass
@@ -189,79 +178,69 @@ class VUnknown(Value):
 
 class CBinding(Command):
     def __init__(self, a: Binding):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class CAssignment(Command):
     def __init__(self, a: Assignment):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class CProveControl(Command):
     def __init__(self, a: ProveControl):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class CBlock(Command):
     def __init__(self, a: Block):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class CNoop(Command):
     pass
 
 class ATuple(Assignment):
     def __init__(self, a: typing.List[Assignment]):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class ASingle(Assignment):
     def __init__(self, var: Variable, val: Value):
-        self.var = var
-        self.val = val
+        dict.__init__(self, var=var, val=val)
 
 class BDeclaration(Binding):
     def __init__(self, var: Variable, t: Type, m: bool):
-        self.var = var
-        self.t = t
-        self.m = m
+        dict.__init__(self, var=var, t=t, m=m)
 
 class BAssignment(Binding):
     def __init__(self, var: Variable, t: Type, m: bool, v: Value):
-        self.var = var
-        self.t = t
-        self.m = m
-        self.v = v
+        dict.__init__(self, var=var, t=t, m=m, v=v)
 
 class BTuple(Binding):
     def __init__(self, a: typing.List[Command]):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class VNamed(Variable):
     def __init__(self, name: str):
-        self.name = name
+        dict.__init__(self, name=name)
 
 class VEmpty(Variable):
     pass
 
 class VArrayElem(Variable):
     def __init__(self, arr_name: str, index: Value):
-        self.arr_name = arr_name
-        self.index = index
+        dict.__init__(self, arr_name=arr_name, index=index)
 
 class VTupleElem(Variable):
     def __init__(self, tup_name: str, index: Value):
-        self.tup_name = tup_name
-        self.index = index
+        dict.__init__(self, tup_name=tup_name, index=index)
 
 class ENumber(Expr):
     def __init__(self, a: int):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class EValue(Expr):
     def __init__(self, a: Value):
-        self.a = a
+        dict.__init__(self, a=a)
 
 class EOp(Expr):
     def __init__(self, a: Expr, op: Opcode, b: Expr):
-        self.a = a
-        self.op = op
-        self.b = b
+        dict.__init__(self, a=a, op=op, b=b)
 
 class OMul(Opcode):
     pass
@@ -282,19 +261,13 @@ class BIf(Block):
     def __init__(self, ifs: typing.List[Bool], blocks:
                  typing.List[typing.List[Command]],
                  el: typing.List[Command]):
-        self.ifs = ifs
-        self.blocks = blocks
-        self.el = el
+        dict.__init__(self, ifs=ifs, blocks=blocks, el=el)
 
 class BForRange(Block):
     def __init__(self, it: Variable, first: Value, last: Value, block:
                  typing.List[Command]):
-        self.it = it
-        self.first = first
-        self.last = last
-        self.block = block
+        dict.__init__(self, it=it, first=first, last=last, block=block)
 
 class BWhile(Block):
     def __init__(self, cond: Bool, block: typing.List[Command]):
-        self.cond = cond
-        self.block = block
+        dict.__init__(self, cond=cond, block=block)
