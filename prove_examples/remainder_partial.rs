ecd9154889bc3d00244013eb14d5dd064501491b
d0ku <darthtyranus666666@gmail.com>
2021-05-04 00:59:09 +0200

    [examples] improved remainder

--- prove_examples/remainder_partial.rs
+++ prove_examples/remainder_partial.rs
@@ -1,12 +1,13 @@
-//%precondition x >= 0 && y > 0
+//%precondition x >= 0 && y >= 0
 //%postcondition quo * y + rem == x
 fn remainder(x: i32, y: i32) -> (i32, i32) {
     // First program proved by Hoare
     let mut quo: i32 = 0;
     let mut rem: i32 = x;
 
-    //%invariant quo * y + rem == x && y > 0 && rem >= 0
-    //%variant rem
+    // No variant, as this program is only partially correct
+    // If y >= 0 then it's possible for rem to loop infinitely
+    //%invariant quo * y + rem == x
     while rem >= y {
         rem = rem - y;
         quo = quo + 1;
