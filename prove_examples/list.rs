6c7d9e50cb620f3b4f7a94a1f5077580e272306a
d0ku <darthtyranus666666@gmail.com>
2021-05-15 02:51:53 +0200

    [examples] clarify the list one

--- prove_examples/list.rs
+++ prove_examples/list.rs
@@ -3,7 +3,7 @@
 fn list(x: &mut [i32], n: usize) {
     let mut i: usize = 0;
     let mut j: i32 = 0;
-    //%invariant i == j && n - i >= 0 && forall y !(y >= 0 && y < i) || x[y] == y
+    //%invariant i == j && n - i >= 0 && (forall y !(y >= 0 && y < i) || x[y] == y)
     //%variant n - i
     while i < n {
         x[i] = j;
